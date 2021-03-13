027c0ac5e8b8da945db7e3c689f18a6cb295c654
Arjan Wooning <arjan@wooning.cz>
2021-03-13 19:02:53 +0100

    Working and tested

--- AlphaEssMonitor.py
+++ AlphaEssMonitor.py
@@ -5,13 +5,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-from const import (
-    DEFAULT_HOST
-)
+import settings
 
 
 class AlphaEssMonitor():
-    def __init__(self, user_name, password, host=DEFAULT_HOST):
+    def __init__(self, user_name, password, host=settings.ALPHAESS_HOST):
+        # print("AlphaEssMonitor: __init__")
         self.user_name = user_name
         self.password = password
         self.host = host
@@ -21,12 +20,12 @@ class AlphaEssMonitor():
     def start(self, driver):
         if self.started:
             return
-
         self.driver = driver
 
         self.started = True
         self.driver.get(self.host)
         self.driver.set_window_size(968, 1030)
+        print("AlphaEssMonitor: login")
         self.driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(2) .el-input__inner").send_keys(self.user_name)
         self.driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(3) .el-input__inner").send_keys(self.password)
         self.driver.find_element(By.CSS_SELECTOR, ".el-button--primary").click()
@@ -34,7 +33,7 @@ class AlphaEssMonitor():
         self.password = None
 
     def stop(self):
-        self.driver.get(self.host + "/Account/Logout")
+        # self.driver.get(self.host + "/logout")
         self.driver.quit()
         self.driver = None
         self.started = False
@@ -48,7 +47,6 @@ class AlphaEssMonitor():
 
         self.driver.refresh()
         WebDriverWait(self.driver, 15000).until(
-        # WebDriverWait(self.driver, 60).until(
             expected_conditions.visibility_of_element_located((By.ID, "tab-2")))
         sleep(10)
         self.driver.find_element(By.ID, "tab-2").click()
@@ -64,6 +62,6 @@ class AlphaEssMonitor():
 
     def get_value(self, id):
         WebDriverWait(self.driver, 5000).until(
-            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".pie-data-container > :nth-child("+id+") tspan")))
+            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".pie-data-container > :nth-child(" + id + ") tspan")))
         sleep(2)
-        return float(re.sub("%|kW", "", self.driver.find_element_by_css_selector(".pie-data-container > :nth-child("+id+") tspan").text))
+        return float(re.sub("%|kW", "", self.driver.find_element_by_css_selector(".pie-data-container > :nth-child(" + id + ") tspan").text))
--- alpha-ess-web_mqtt.py
+++ alpha-ess-web_mqtt.py
@@ -1,91 +1,51 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import os
 import signal
 import time
-import simplejson as json
-import socket
+import serial
+import _thread
+import traceback
+from queue import Queue
+import json
 import paho.mqtt.publish as mqtt_publish
 import paho.mqtt.client as mqtt_client
-import psutil
-import os.path
-import subprocess
-from subprocess import PIPE, Popen
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import shutil
 
 # external files/classes
 import logger
+import serviceReport
 import settings
+import AlphaEssMonitor
 
-#System check
-ACTION_NOTHING   = 0
-ACTION_RESTART   = 1
-
+sendQueue = Queue(maxsize=0)
 current_sec_time = lambda: int(round(time.time()))
+current_milli_time = lambda: int(round(time.time() * 1000))
+oldTimeout = 0
 
-serverStatus = {"cpuTemp": "0.0", "cpuUsage": "0.0", "totalProcesses": "0.0", "nrOfZombies": "0.0", "swapUsage": "0.0", "diskUsage": "0.0", "memUsage": "0.0"}
 exit = False
-pingFailCount = 0
-oldTimeout = current_sec_time()
-checkMsg = 'OK'
-checkFail = False
-checkAction = ACTION_NOTHING
-checkReport = {}
-
-hostnameToServerTopic = {
-    'cb2-live':   'CB2Live',
-    'rpi-home':   'RPiHome',
-    'rpi-infra':  'RPiInfra',
-    'rpi-video1': 'RPiVideo1',
-    'rpi-video2': 'RPiVideo2'
-}
-
-hostname = socket.gethostname().lower()
-serverTopic = hostnameToServerTopic[hostname]
-
-mqttTopicCheck  = settings.MQTT_TOPIC_CHECK  % serverTopic
-mqttTopicReport = settings.MQTT_TOPIC_REPORT  % serverTopic
+chromeDriver = None
+monitor = None
 
 
 def signal_handler(_signal, frame):
     global exit
+
     print('You pressed Ctrl+C!')
     exit = True
-
-
-#Called by mqtt
-def on_message_check(client, userdata, msgJson):
-    #print("on_message_check: " + msgJson.topic + ": " + str(msgJson.payload))
-    sendCheckReportToHomeLogic(checkFail, checkAction, checkMsg)
-
-
-#Send the report to the Home Logic system checker
-def sendCheckReportToHomeLogic(fail, action, msg):
-    global checkMsg
-    global checkFail
-    global oldTimeout
-
-    checkMsg = msg
-    checkFail = fail
-    checkReport['checkFail']   = checkFail
-    checkReport['checkAction'] = checkAction
-    checkReport['checkMsg']    = checkMsg
-    mqtt_publish.single(mqttTopicReport, json.dumps(checkReport), qos=1, hostname=settings.MQTT_ServerIP)
-
-    # Reset the RFLink Rx timeout timer
-    oldTimeout = current_sec_time()
-
-
-#Don't wait for the Home Logic system checker, report it directly
-def sendFailureToHomeLogic(checkAction, checkMsg):
-    sendCheckReportToHomeLogic(True, checkAction, checkMsg)
 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print(("%s: MQTT Client connected successfully" % (time.ctime(time.time()))))
-        client.subscribe([(mqttTopicCheck, 1)])
+        print("MQTT Client connected successfully")
+        client.subscribe([(settings.MQTT_TOPIC_OUT, 1), (settings.MQTT_TOPIC_CHECK, 1)])
     else:
-        print(("%s: ERROR: MQTT Client connected with result code %s " % (time.ctime(time.time()), str(rc))))
+        print(("ERROR: MQTT Client connected with result code %s " % str(rc)))
 
 
 # The callback for when a PUBLISH message is received from the server
@@ -93,22 +53,93 @@ def on_message(client, userdata, msg):
     print(('ERROR: Received ' + msg.topic + ' in on_message function' + str(msg.payload)))
 
 
-def getCpuTemperature():
-    # Test if the i2c temp sensor exists (for example on CubieBoard2)
-    if os.path.isfile("/sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input"):
+def on_message_homelogic(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+    # topics = msg.topic.split("/")
 
-        with open('/sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input') as f:
-            for line in f:
-                return (float(line.rstrip('\n')) / 1000)
-    if os.path.isfile("/sys/devices/virtual/thermal/thermal_zone0/temp"):
-        with open('/sys/devices/virtual/thermal/thermal_zone0/temp') as f:
-            for line in f:
-                return (float(line.rstrip('\n')) / 1000)
-    else:
-    # Otherwise use the RaspberryPi's vcgencmd temp command
-        process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
-        output, _error = process.communicate()
-        return float(output[output.index(b'=') + 1:output.rindex(b"'")])
+    # deviceName = topics[2] #huis/RFXtrx/KaKu-12/out
+    # cmnd = deviceName.split("-") #KaKu-12
+
+
+def alphaEssThread():
+    global oldTimeout
+    global chromeDriver
+    global monitor
+    global exit
+
+    oldTimeout = current_sec_time()
+
+    try:
+        print("Start Chrome driver")
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.binary_location = shutil.which("chromium-browser")
+        chromeDriver = webdriver.Chrome(options=chrome_options, executable_path=shutil.which("chromedriver"))
+
+    # Handle other exceptions and print the error
+    except Exception as arg:
+        print("%s" % str(arg))
+        traceback.print_exc()
+
+        #Report failure to Home Logic system check
+        serviceReport.sendFailureToHomeLogic(serviceReport.ACTION_NOTHING, 'Failed to open Chrome Driver')
+        exit = True
+        return
+
+    print("Init AlphaEss monitor")
+    monitor = AlphaEssMonitor.AlphaEssMonitor(settings.ALPHAESS_USERNAME, settings.ALPHAESS_PASSWORD)
+
+    print("Start AlphaEss monitor")
+    monitor.start(chromeDriver)
+
+    while not exit:
+        try:
+            # print("Get monitor data")
+            monitor_data = monitor.get_data()
+
+            for key in monitor_data:
+                # print("Monitor data key: %s" % key)
+                # mqtt_client.publish(base_topic + "/" + key, monitor_data[key])
+                # mqtt_client.publish("huis/AlphaESS/%s/solar" % key, monitor_data[key])
+                mqtt_publish.single("huis/AlphaESS/%s/solar" % key, monitor_data[key], hostname=settings.MQTT_ServerIP, retain=True)
+                serviceReport.systemWatchTimer = current_sec_time()
+
+            # print("Waiting %d seconds" % settings.ALPHAESS_WAIT)
+            time.sleep(settings.ALPHAESS_WAIT)
+
+            # Check if there is any message to send to AlphaEss.com
+            if not sendQueue.empty():
+                sendMsg = sendQueue.get_nowait()
+                #print "SendMsg:", sendMsg
+                if sendMsg != "":
+                    print("SendMsg: Not implemented yet")
+
+        # In case the message contains unusual data
+        except ValueError as arg:
+            print(arg)
+            traceback.print_exc()
+            time.sleep(1)
+
+        # Quit the program by Ctrl-C
+        except KeyboardInterrupt:
+            print("Program aborted by Ctrl-C")
+            exit()
+
+        # Handle other exceptions and print the error
+        except Exception as arg:
+            print("%s" % str(arg))
+            traceback.print_exc()
+            time.sleep(10)
+
+
+def print_time(delay):
+    count = 0
+    while count < 5:
+        time.sleep(delay)
+        count += 1
+        print("%s" % (time.ctime(time.time())))
 
 
 ###
@@ -119,96 +150,31 @@ logger.initLogger(settings.LOG_FILENAME)
 # Init signal handler, because otherwise Ctrl-C does not work
 signal.signal(signal.SIGINT, signal_handler)
 
-readSensorTimer = 5  # Read sensors after 10 sec
-
-serverTopic = hostnameToServerTopic[hostname]
-print(("Script runs on host: %s, serverTopic: %s" % (hostname, serverTopic)))
-
 # Give Home Assistant and Mosquitto the time to startup
 time.sleep(2)
 
 # First start the MQTT client
 client = mqtt_client.Client()
+client.message_callback_add(settings.MQTT_TOPIC_OUT,       on_message_homelogic)
+client.message_callback_add(settings.MQTT_TOPIC_CHECK,     serviceReport.on_message_check)
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(settings.MQTT_ServerIP, settings.MQTT_ServerPort, 60)
-client.message_callback_add(mqttTopicCheck, on_message_check)
 client.loop_start()
 
+# Create the alphaEssThread
+try:
+    # thread.start_new_thread( print_time, (60, ) )
+    _thread.start_new_thread(alphaEssThread, ())
+except Exception:
+    print("Error: unable to start the alphaEssThread")
+
+
 while not exit:
-    readSensorTimer = readSensorTimer + 1
-    # Read and send the sensor data every 600s=10min
-    if readSensorTimer >= 6:
-        readSensorTimer = 0
+    time.sleep(6)  # 60s
 
-        # CPU temp
-        cpuTemp = getCpuTemperature()
-        #print ("CPU Temp: %s" % cpuTemp)
+if monitor is not None:
+    monitor.stop()
+    print("Stopped AlphaEss monitor")
 
-        time.sleep(1)  # 1s
-        # Memory Usage
-        mem = psutil.virtual_memory()
-        memUsage = 100.0 - float(mem.available) / float(mem.total) * 100.0
-        memUsage = round(memUsage, 1)
-        #print ("Memory Usage: %2.1f %%, available: %d %%, total: %d %%" % (memUsage, mem.available, mem.total))
-
-        time.sleep(1)  # 2s
-        # Disk usage disk "/"
-        diskUsage = psutil.disk_usage('/').percent
-        #print ("Disk Usage: %s" % diskUsage)
-
-        time.sleep(1)  # 3s
-        # CPU Usage Meet 1 sec
-        cpuUsage = psutil.cpu_percent(interval=1)
-        #print ("CPU Usage: %s" % cpuUsage)
-        # print ("-----")
-
-        time.sleep(1)  # 4s
-        result = subprocess.check_output('ps axo stat,ppid,pid,comm | grep -w defunct | wc -l', shell=True)
-        nrOfZombies = int(result.decode().rstrip('\n'))
-        #print("Nr of zombie processes: " + str(nrOfZombies))
-
-        time.sleep(1)  # 5s
-        result = subprocess.check_output('ps -A --no-headers | wc -l', shell=True)
-        totalProcesses = int(result.decode().rstrip('\n'))
-        #print("Total Nr of processes: " + str(totalProcesses))
-
-        time.sleep(1)  # 6s
-        #             total       used       free
-        #Swap:       524284          0     524284
-        result = subprocess.check_output("free | grep 'Swap'", shell=True)
-        i = 0
-        swap = [0] * 4
-        for s in result.split():
-            if s.isdigit():
-                swap[i] = int(s)
-                i = i + 1
-        if swap[0] != 0:
-            swapUsage = (swap[1] / swap[0]) * 100
-            #print("swapUsage: %1.0f%%" % (swapUsage))
-        else:
-            swapUsage = -1
-            #print("swapUsage not active")
-
-        time.sleep(1)  # 7s
-        serverStatus['cpuTemp'] = cpuTemp
-        serverStatus['cpuUsage'] = cpuUsage
-        serverStatus['totalProcesses'] = totalProcesses
-        serverStatus['nrOfZombies'] = nrOfZombies
-        serverStatus['swapUsage'] = ("%1.0f" % swapUsage)
-        serverStatus['diskUsage'] = diskUsage
-        serverStatus['memUsage'] = memUsage
-
-        mqtt_publish.single("huis/Board/%s/server" % serverTopic, json.dumps(serverStatus, separators=(', ', ':')), hostname="192.168.5.248", retain=True)
-
-        time.sleep(3)  # 3s
-    # else:
-    #     if hostname == 'cb2-live':
-    #         # Check the RFLink Rx timeout
-    #         if (current_sec_time() - oldTimeout) > 900:
-    #             #Report failure to Home Logic system check
-    #             sendFailureToHomeLogic(ACTION_NOTHING, '15 minutes nothing received from systemWatch, likely network interface down, going to reboot')
-    #             time.sleep(2)  # Wait for message is send
-    #             os.system("reboot")
-
-        time.sleep(10)  # 10s
+print("Clean exit!")
